f51818b6237a7c1f40d79ba9281b1ef00aec0a53
lbussy <lee@bussy.org>
2019-04-14 09:28:47 -0500

    Change actuator designations, add an actuator

--- include/Pins.h
+++ include/Pins.h
@@ -33,159 +33,157 @@ license and credits. */
 
 #if BREWPI_STATIC_CONFIG == BREWPI_SHIELD_REVA
 
-#ifndef beerSensorPin
-#define beerSensorPin A5 // OneWire 1
-#endif
+    #ifndef beerSensorPin
+        #define beerSensorPin A5 // OneWire 1
+    #endif
 
-#ifndef fridgeSensorPin
-#define fridgeSensorPin A4 // OneWire 2
-#endif
+    #ifndef fridgeSensorPin
+        #define fridgeSensorPin A4 // OneWire 2
+    #endif
 
-#ifndef coolingPin
-#define coolingPin 6
-#endif
+    #ifndef coolingPin
+        #define coolingPin 6
+    #endif
 
-#ifndef heatingPin
-#define heatingPin 5
-#endif
+    #ifndef heatingPin
+        #define heatingPin 5
+    #endif
 
-#ifndef doorPin
-#define doorPin 4
-#endif
+    #ifndef doorPin
+            #define doorPin 4
+    #endif
 
-#ifndef alarmPin
-#define alarmPin 3
-#endif
+    #ifndef alarmPin
+        #define alarmPin 3
+    #endif
 
-#ifndef lcdLatchPin
-#define lcdLatchPin 10
-#endif
+    #ifndef lcdLatchPin
+        #define lcdLatchPin 10
+    #endif
 
-// If you change the interrupt pins, you will also have to review the interrupt vectors of the rotary encoder
-#define rotarySwitchPin 7 // INT6 on leo or PCINT23 on uno
-#define rotaryAPin 8      // PCINT4 on leo or PCINT0 on uno
-#define rotaryBPin 9      // PCINT5 on leo or PCINT1 on uno
+    // If you change the interrupt pins, you will also have to review the
+    // interrupt vectors of the rotary encoder
+    #define rotarySwitchPin 7 // INT6 on leo or PCINT23 on uno
+    #define rotaryAPin 8      // PCINT4 on leo or PCINT0 on uno
+    #define rotaryBPin 9      // PCINT5 on leo or PCINT1 on uno
 
-#define BREWPI_INVERT_ACTUATORS 1
+    #define BREWPI_INVERT_ACTUATORS 1
 
 #elif BREWPI_STATIC_CONFIG == BREWPI_SHIELD_REVC
 
-#ifndef oneWirePin
-#define oneWirePin A4
-#endif
+    #ifndef oneWirePin
+        #define oneWirePin A4
+    #endif
 
-#ifndef actuatorPin1
-#define actuatorPin1 2
-#endif
+    #ifndef actuatorPin1
+        #define actuatorPin1 2
+    #endif
 
-#ifndef actuatorPin2
-#define actuatorPin2 5
-#endif
+    #ifndef actuatorPin2
+        #define actuatorPin2 5
+    #endif
 
-#ifndef actuatorPin3
-#define actuatorPin3 6
-#endif
+    #ifndef actuatorPin3
+        #define actuatorPin3 6
+    #endif
 
-#ifndef actuatorPin4
-#define actuatorPin4 A5
-#endif
+    #ifndef actuatorPin4
+        #define actuatorPin4 A5
+    #endif
 
-#ifndef doorPin
-#define doorPin 4
-#endif
+    #ifndef doorPin
+        #define doorPin 4
+    #endif
 
-#ifndef alarmPin
-#define alarmPin 3
-#endif
+    #ifndef alarmPin
+        #define alarmPin 3
+    #endif
 
-#ifndef lcdLatchPin
-#define lcdLatchPin 10
-#endif
+    #ifndef lcdLatchPin
+        #define lcdLatchPin 10
+    #endif
 
-// If you change the interrupt pins, you will also have to review the interrupt vectors of the rotary encoder
-#define rotarySwitchPin 7 // INT6 on leo or PCINT23 on uno
-#define rotaryAPin 8      // PCINT4 on leo or PCINT0 on uno
-#define rotaryBPin 9      // PCINT5 on leo or PCINT1 on uno
+    // If you change the interrupt pins, you will also have to review the
+    // interrupt vectors of the rotary encoder
+    #define rotarySwitchPin 7 // INT6 on leo or PCINT23 on uno
+    #define rotaryAPin 8      // PCINT4 on leo or PCINT0 on uno
+    #define rotaryBPin 9      // PCINT5 on leo or PCINT1 on uno
 
-#define BREWPI_INVERT_ACTUATORS 1
+    #define BREWPI_INVERT_ACTUATORS 1
 
 #elif BREWPI_STATIC_CONFIG == BREWPI_SHIELD_DIY
 
-// pins
-#define beerSensorPin 10
-#define fridgeSensorPin 11
+    // pins
+    #define beerSensorPin 10
+    #define fridgeSensorPin 11
 
-// Pay attention when changing the pins for the rotary encoder.
-// They should be connected to external interrupt INT0, INT1 and INT3
+    // Pay attention when changing the pins for the rotary encoder.
+    // They should be connected to external interrupt INT0, INT1 and INT3
 
-#define rotaryAPin 2      // INT1
-#define rotaryBPin 1      // INT3
-#define rotarySwitchPin 0 // INT2
+    #define rotaryAPin 2      // INT1
+    #define rotaryBPin 1      // INT3
+    #define rotarySwitchPin 0 // INT2
 
-#define coolingPin 12
-#define heatingPin 13
-#define doorPin A5
+    #define coolingPin 12
+    #define heatingPin 13
+    #define doorPin A5
 
-#define DISP_RS 9
-#define DISP_RW 8
-#define DISP_EN 7
-#define DISP_D4 6
-#define DISP_D5 5
-#define DISP_D6 4
-#define DISP_D7 3
+    #define DISP_RS 9
+    #define DISP_RW 8
+    #define DISP_EN 7
+    #define DISP_D4 6
+    #define DISP_D5 5
+    #define DISP_D6 4
+    #define DISP_D7 3
 
-#define BREWPI_INVERT_ACTUATORS 0
+    #define BREWPI_INVERT_ACTUATORS 0
 
 #elif BREWPI_STATIC_CONFIG == BREWPI_SHIELD_I2C
 
-#ifndef oneWirePin
-#define oneWirePin A0  // A4 in Rev C
-#endif
+    #ifndef oneWirePin
+        #define oneWirePin A0 // OneWire Sensors
+    #endif
 
-#ifndef actuatorPin1
-#define actuatorPin1 2  // NC
-#endif
+    #ifndef actuatorPin1
+        #define actuatorPin1 5 // Generally 'Cool'
+    #endif
 
-#ifndef actuatorPin2
-#define actuatorPin2 5
-#endif
+    #ifndef actuatorPin2
+        #define actuatorPin2 6 // Generally 'Heat'
+    #endif
 
-#ifndef actuatorPin3
-#define actuatorPin3 6
-#endif
+    #ifndef actuatorPin3
+        #define actuatorPin3 2 // Avail for fan or light
+    #endif
 
-#ifndef actuatorPin4
-#define actuatorPin4 A1  // TODO:  Check this (A5 in Rev C)
-#endif
+    #ifndef actuatorPin4
+        #define actuatorPin4 10 // Avail for fan or light
+    #endif
 
-#ifndef doorPin
-#define doorPin 4
-#endif
+    #ifndef doorPin
+        #define doorPin 4 // Connects to fridge door switch
+    #endif
 
-#ifndef alarmPin
-#define alarmPin 3
-#endif
+    #ifndef alarmPin
+        #define alarmPin 3 // Piezo buzzer
+    #endif
 
-#ifndef lcdLatchPin
-#define lcdLatchPin 10
-#endif
+    // If you change the interrupt pins, you will also have to review the
+    // interrupt vectors of the rotary encoder
+    #define rotarySwitchPin 7 // INT6 on leo or PCINT23 on uno
+    #define rotaryAPin 8      // PCINT4 on leo or PCINT0 on uno
+    #define rotaryBPin 9      // PCINT5 on leo or PCINT1 on uno
 
-// If you change the interrupt pins, you will also have to review the
-// interrupt vectors of the rotary encoder
-#define rotarySwitchPin 7 // INT6 on leo or PCINT23 on uno
-#define rotaryAPin 8      // PCINT4 on leo or PCINT0 on uno
-#define rotaryBPin 9      // PCINT5 on leo or PCINT1 on uno
+    // IIC pin definitions are not used, they are here for documentation
+    #define IIC_SDA  A4  
+    #define IIC_SCL  A5
 
-// IIC pin definitions are not used, they are here for documentation
-#define IIC_SDA  A4  
-#define IIC_SCL  A5
-
-#define BREWPI_INVERT_ACTUATORS 1
+    #define BREWPI_INVERT_ACTUATORS 1
 
 #endif
 
 // You can use the internal pull-up resistors instead of external ones for
 // the doorPin and the rotary encoder pins
 #ifndef USE_INTERNAL_PULL_UP_RESISTORS
-#define USE_INTERNAL_PULL_UP_RESISTORS 1
+    #define USE_INTERNAL_PULL_UP_RESISTORS 1
 #endif

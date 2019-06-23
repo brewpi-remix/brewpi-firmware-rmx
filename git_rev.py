9a1e2a9036ac3bcfa83c3b152f67166ca1de83bf
lbussy <lee@bussy.org>
2019-06-23 10:00:23 -0500

    Change to python 3 format

--- git_rev.py
+++ git_rev.py
@@ -41,5 +41,5 @@ revcmd = "git log --pretty=format:'%h' -n 1"
 commit = subprocess.check_output(revcmd, shell=True).decode().strip()
 
 # Make both available for use in the defines
-print "-DPIO_SRC_TAG=%s" % version
-print "-DPIO_SRC_REV=%s" % commit
+print("-DPIO_SRC_TAG={0}".format(version))
+print("-DPIO_SRC_REV={0}".format(commit))
